83b930a57bf1adc2a8d40e0faacfeb1683a37080
Alex <alexstampfl@gmail.com>
2024-09-14 06:47:19 -0500

    minor changes

--- BankAccount.java
+++ BankAccount.java
@@ -5,7 +5,6 @@ public class BankAccount {
     private String lastName;
     private int accountID;
 
-
     // constructor
     public BankAccount(String firstName, String lastName, int accountID){
         this.firstName = firstName;
@@ -13,7 +12,6 @@ public class BankAccount {
         this.accountID = accountID;
         this.balance = 0.0; // Initialize balance to zero
     }
-
     public void deposit(double amount){
         if (amount > 0){
             balance += amount;
@@ -21,7 +19,6 @@ public class BankAccount {
             System.out.println("Deposit amount must be positive");
         }
     }
-
     public void withdrawal(double amount){
         if (amount > 0 && amount <= balance){
             balance -= amount;
@@ -29,7 +26,6 @@ public class BankAccount {
             System.out.println("Insufficient funds or invalid amount.");
         }
     }
-
     // getters and setters
     public String getFirstName() {
         return firstName;
@@ -37,34 +33,29 @@ public class BankAccount {
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
     public String getLastName() {
         return lastName;
     }
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
     public int getAccountID() {
         return accountID;
     }
     public void setAccountID(int accountID) {
         this.accountID = accountID;
     }
-
     public double getBalance() {
         return balance;
     }
     public void setBalance(double balance) {
         this.balance = balance;
     }
-
     // print account info & balance
     public void accountSummary(){
         System.out.printf("Account information: %s %s, Account ID: %d%n", firstName, lastName, accountID);
         System.out.printf("Current balance: $%.2f%n", balance);
     }
-
     // main method
     public static void main(String[] args){
         System.out.println("This is the BankAccount class.");
